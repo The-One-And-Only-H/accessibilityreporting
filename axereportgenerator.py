09f9448166e0bfee3decfa257b26a6ae62a6c470
H <juana.galan@outlook.com>
2020-01-31 16:54:37 +0000

    Replaces CSV with openpyxl for formatting purposes

--- axereportgenerator.py
+++ axereportgenerator.py
@@ -1,13 +1,18 @@
 import logging
+
 import yaml
-import csv
+
+import openpyxl
+from openpyxl.styles import Font
 
 from selenium import webdriver
 from selenium.webdriver import ChromeOptions
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as SeleniumExpectedConditions
 from selenium.webdriver.common.by import By
+
 from axe_selenium_python import Axe
+
 from argparse import ArgumentParser
 
 # Log more information messages to the shell
@@ -56,13 +61,13 @@ def main():
     summary = aggregateResults(results)
     emitResults(summary)
 
-# Optional argument to see script running in the browser
+# Optional argument to see script running in the brosheeter
 
 
 def parseCommandLine():
     parser = ArgumentParser()
     parser.add_argument('--visible', action='store_true',
-                        help='display browser')
+                        help='display brosheeter')
     parser.add_argument(
         'input', help='runs script against chosen list of URLs')
     args = parser.parse_args()
@@ -77,26 +82,26 @@ def loadInputFile(args):
     return data
 
 
-# Hide Selenium running in browser when running script
+# Hide Selenium running in brosheeter when running script
 
 
 def setupHeadlessChrome(args):
     chrome_options = ChromeOptions()
     if not args.visible:
         chrome_options.add_argument("--headless")
-    browser = webdriver.Chrome(
+    brosheeter = webdriver.Chrome(
         executable_path="chromedriver", options=chrome_options)
 
-    return browser
+    return brosheeter
 
 
-def loginToPage(browser, url):
+def loginToPage(brosheeter, url):
     logger.info('Logging into %s', url)
 
-    browser.get(url)
+    brosheeter.get(url)
 
     # This will await the first load on the login page, based on that 'maincontent' is drawn
-    WebDriverWait(browser, 10).until(
+    WebDriverWait(brosheeter, 10).until(
         SeleniumExpectedConditions.presence_of_element_located(
             (By.ID, "maincontent"))
     )
@@ -106,24 +111,24 @@ def loginToPage(browser, url):
     # password = Password1
 
     # Here we do find the first input element and inputs some text into it
-    usernameElement = browser.find_element_by_name("userNameOrEmail")
-    passwordElement = browser.find_element_by_name("password")
+    usernameElement = brosheeter.find_element_by_name("userNameOrEmail")
+    passwordElement = brosheeter.find_element_by_name("password")
 
     usernameElement.send_keys('h@neverbland.com')
     passwordElement.send_keys('Password1')
 
     # Accept the cookies
-    browser.find_elements_by_xpath(
+    brosheeter.find_elements_by_xpath(
         "//button//*[contains(text(), 'Accept')]")[0].click()
-    WebDriverWait(browser, 10).until_not(
+    WebDriverWait(brosheeter, 10).until_not(
         SeleniumExpectedConditions.presence_of_element_located(
             (By.XPATH, "//button//*[contains(text(), 'Accept')]"))
     )
 
     # Press the login button
-    browser.find_elements_by_xpath(
+    brosheeter.find_elements_by_xpath(
         "//button//*[contains(text(), 'Log in')]")[0].click()
-    WebDriverWait(browser, 10).until_not(
+    WebDriverWait(brosheeter, 10).until_not(
         SeleniumExpectedConditions.presence_of_element_located(
             (By.XPATH, "//button//*[contains(text(), 'Log in')]"))
     )
@@ -131,8 +136,8 @@ def loginToPage(browser, url):
 # Detect element on landing page after log in
 
 
-def awaitFirstDrawOnPage(browser):
-    WebDriverWait(browser, 10).until(
+def awaitFirstDrawOnPage(brosheeter):
+    WebDriverWait(brosheeter, 10).until(
         SeleniumExpectedConditions.presence_of_element_located(
             (By.XPATH, "//h2[contains(text(), 'Hi')]"))
     )
@@ -144,22 +149,22 @@ def awaitFirstDrawOnPage(browser):
 def processPages(args, data):
     results = []
     for page in data['pages']:
-        browser = setupHeadlessChrome(args)
+        brosheeter = setupHeadlessChrome(args)
         if page.get('require_login'):
-            loginToPage(browser, data['login']['url'])
-            awaitFirstDrawOnPage(browser)
-        results.append(runAxeReport(browser, args, page))
-        closeBrowser(browser)
+            loginToPage(brosheeter, data['login']['url'])
+            awaitFirstDrawOnPage(brosheeter)
+        results.append(runAxeReport(brosheeter, args, page))
+        closeBrosheeter(brosheeter)
     return results
 
 # Run Axe from the command line
 
 
-def runAxeReport(browser, args, page):
+def runAxeReport(brosheeter, args, page):
     logger.info("Running Axe against %s", page['url'])
     pageUrl = page['url']
-    browser.get(pageUrl)
-    axe = Axe(browser)
+    brosheeter.get(pageUrl)
+    axe = Axe(brosheeter)
     # Inject axe-core javascript into page
     axe.inject()
     # Run axe accessibility checks
@@ -167,8 +172,8 @@ def runAxeReport(browser, args, page):
     return results
 
 
-def closeBrowser(browser):
-    browser.quit()
+def closeBrosheeter(brosheeter):
+    brosheeter.quit()
 
 # Filter through problems flagged by the Axe report and collate duplicates
 
@@ -191,14 +196,34 @@ def emitResults(summary):
 
     problems.sort(key=getCount, reverse=True)
 
-    # Writes flagged items as CSV file
-    with open('report.csv', 'w') as f:
-        w = csv.writer(f)
-        w.writerow(["Count", "Priority", "Title", "Description", "More info"])
-        for p in problems:
-            w.writerow([p.count, p.impact, p.help, p.description, p.helpUrl])
+    ''' Create a new blank Workbook to record flagged items '''
+    wb = openpyxl.Workbook() 
+    
+    ''' Get workbook active sheet '''
+    ws = wb.active 
+
+    ''' Write to the cells '''     
+    ws.append(["Count", "Priority", "Title", "Description", "More info"])
+
+    for p in problems:
+        ws.append([p.count, p.impact, p.help, p.description, p.helpUrl])
+    
+    ''' Set the height of the row '''
+    #ws.row_dimensions.height = 20
+    
+    ''' Set the width of the row '''
+    #ws.row_dimensions.width = 100
+
+    header = Font(color='00FF0000', bold=True)
+
+    ''' Enumerate the cells in the first row '''
+    for cell in ws["1:1"]:
+        cell.font = header
+    
+    ''' Save the file '''
+    wb.save('report.xlsx') 
 
 
-# Execute main only if script is being executed, not imported
+''' Execute main only if script is being executed, not imported '''
 if __name__ == '__main__':
     main()
